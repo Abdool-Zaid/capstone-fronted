e8be766824cdae323e83450b2135f2ca382cc917
unknown <aztoorabally7447@gmail.com>
2022-08-25 10:04:00 +0200

    'state'

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,11 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user:[],
+    users:[],
+    posts:[],
+    messages:[],
+    groupMessages:[],
   },
   getters: {
   },
