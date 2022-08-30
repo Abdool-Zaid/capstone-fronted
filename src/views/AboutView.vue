74479081ab615c33ad04b6c2d2e9c2953ee8e28d
unknown <aztoorabally7447@gmail.com>
2022-08-30 14:00:48 +0200

    'try_my_luck'

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,20 +1,20 @@
 <template>
 <div class="container d-flex  justify-content-center align-items-center">
   <div >
-    <form>
+    <form v-on:submit.prevent="sendUserData()">
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <label for="email" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
       </div>
       <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password">
       </div>
       <div class="mb-3 form-check">
         <input type="checkbox" class="form-check-input" id="exampleCheck1">
         <label class="form-check-label" for="exampleCheck1">keep me logged in</label>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary" >Submit</button>
     </form>
   </div>
   </div>
@@ -27,8 +27,63 @@
 </style>
 <script>
   export default{
-    props:{
-      
+    data() {
+    return {
+        user: this.$store.state.user
+    }
+    },
+    methods:{
+      async VerifyUser() {
+        const response = await fetch(
+          "https://tubular-malasada-d6c6b7.netlify.app/users/verify",
+          {
+            method: "GET",
+            // mode:"no-cors",
+      headers: {
+        "Content-type": "application/json",
+        "x-auth-token": JSON.parse(localStorage.token),
+      },
+    }
+  );
+  let data = await response.json();
+  if (data.msg == "Unauthorized Access!") {
+    // alert("login first");
+    console.log(data.msg);
+  } else {
+    localStorage.setItem("UserName", JSON.stringify(data.user.username));
+    localStorage.setItem("UserID", JSON.stringify(data.user.id));
+    localStorage.setItem("UserIntro", JSON.stringify(data.user.intro));
+    localStorage.setItem("UserProfile", JSON.stringify(data.user.profile));
+    alert(`welcome `+ await JSON.parse(localStorage.UserName))
+
+  }
+},
+
+async sendUserData() {
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+  fetch(
+    "https://tubular-malasada-d6c6b7.netlify.app/.netlify/functions/api/users/login",
+    {
+      method: "POST",
+      // mode:"no-cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // mode: "no-cors",
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      localStorage.setItem("token", JSON.stringify(data.token));
+      VerifyUser();
+    });
+}
+
     }
   }
 </script>
