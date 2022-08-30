a4cadf4a8274e0562618e189e87c0694fba0248d
unknown <aztoorabally7447@gmail.com>
2022-08-30 16:20:22 +0200

    'home'

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,24 +1,20 @@
 <template>
   <div class="container d-flex justify-content-center align-items-center">
     <div>
-      <form v-on:submit.prevent="sendUserData()">
+      <form v-on:submit.prevent="sendUserData(),VerifyUser() ">
         <div class="mb-3">
-          <label for="email" class="form-label">Email address</label>
-          <input
-            type="email"
-            class="form-control"
-            id="email"
-            v-model="email"
-            aria-describedby="emailHelp"
-          />
+          <label for="email" class="form-label">email address</label>
+          <input type="email" class="form-control" v-model="email" />
         </div>
         <div class="mb-3">
           <label for="password" class="form-label">Password</label>
-          <input type="password" class="form-control" id="password" v-model="passwordHash " />
+          <input type="password" class="form-control" v-model="passwordHash" />
         </div>
         <div class="mb-3 form-check">
           <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">keep me logged in</label>
+          <label class="form-check-label" for="exampleCheck1"
+            >keep me logged in</label
+          >
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
@@ -36,8 +32,8 @@ export default {
   data() {
     return {
       user: this.$store.state.user,
-      email:this.email,
-      passwordHash:this.passwordHash,
+      passwordHash: "",
+      email: "",
     };
   },
   methods: {
@@ -53,36 +49,46 @@ export default {
           },
         }
       );
-      let data = await response.json();
-      if (data.msg == "Unauthorized Access!") {
+      let UserData = await response.json();
+      if (UserData.msg == "Unauthorized Access!") {
         // alert("login first");
-        console.log(data.msg);
+        console.log(UserData.msg);
       } else {
-        localStorage.setItem("UserName", JSON.stringify(data.user.username));
-        localStorage.setItem("UserID", JSON.stringify(data.user.id));
-        localStorage.setItem("UserIntro", JSON.stringify(data.user.intro));
-        localStorage.setItem("UserProfile", JSON.stringify(data.user.profile));
+        localStorage.setItem(
+          "UserName",
+          JSON.stringify(UserData.user.username)
+        );
+        localStorage.setItem("UserID", JSON.stringify(UserData.user.id));
+        localStorage.setItem("UserIntro", JSON.stringify(UserData.user.intro));
+        localStorage.setItem(
+          "UserProfile",
+          JSON.stringify(UserData.user.profile)
+        );
         alert(`welcome ` + (await JSON.parse(localStorage.UserName)));
       }
     },
 
     async sendUserData() {
-      fetch("https://tubular-malasada-d6c6b7.netlify.app/.netlify/functions/api/users/login",
-        { 
+      fetch(
+        "https://tubular-malasada-d6c6b7.netlify.app/.netlify/functions/api/users/login",
+        {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            email: email,
-            passwordHash: passwordHash,
+            email: this.email,
+            passwordHash: this.passwordHash,
           }),
         }
       )
         .then((res) => res.json())
         .then((tokendata) => {
-          localStorage.setItem("token", JSON.stringify(tokendata.token));
-          VerifyUser();
+          if (tokendata.error) {
+            alert(tokendata.error);
+          } else {
+            localStorage.setItem("token", JSON.stringify(tokendata.token));
+          }
         });
     },
   },
