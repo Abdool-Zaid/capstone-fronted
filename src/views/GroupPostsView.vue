1becb38fdc465eaabec9bb899d780f06881d219c
unknown <aztoorabally7447@gmail.com>
2022-09-07 15:01:19 +0200

    'break'

--- src/views/GroupPostsView.vue
+++ src/views/GroupPostsView.vue
@@ -4,14 +4,19 @@
     <div id="pageSplitter" class="d-flex">
       <div id="controllers">
         <input type="text" placeholder="search controller" />
-        <div >
+        <div>
           <p>carousel controlling carousel</p>
-       
         </div>
       </div>
-      <div id="emoters" >
-        <div class="contactCard " v-for="(user, index) in users" :key="user" aria-live="assertive">
+      <div id="emoters">
+        <div
+          class="contactCard"
+          v-for="(user, index) in users"
+          :key="user"
+          aria-live="assertive"
+        >
           <h4>{{ user.username }}</h4>
+          <h4>{{ user.id }}</h4>
           <img
             :src="user.profile"
             alt="user Profile picture"
@@ -19,103 +24,57 @@
           />
           <div class="controlButtons d-flex justify-content-center">
             <p
-            type="button"
-            class="btn"
-            data-bs-toggle="modal"
-            :data-bs-target="'#modalNumber' + index"
+              type="button"
+              class="btn"
+              data-bs-toggle="modal"
+              :data-bs-target="'#modalNumber' + index"
+              @click="passId(user.id)"
             >
-            my posts
-          </p>
-          <a :href="`mailto:${user.email}`"  target="_blank" rel="noopener noreferrer">Send Email</a>|
-          <a href="mailto:aztoorabally7447@gmail.com"> mail me </a>
-        </div>
-        
-        <!-- Modal -->
-        <div
+              my posts
+            </p>
+            <a
+              :href="`mailto:${user.email}`"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Send me an Email</a
+            >
+          </div>
+
+          <!-- Modal -->
+          <div
             class="modal fade"
             :id="'modalNumber' + index"
             tabindex="1"
-            :aria-labelledby="'m' + index+ 'Label'"
+            :aria-labelledby="'m' + index + 'Label'"
           >
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" :id="'modalNumber' + index+ 'Label'">
-                  post {{ index}}
-                </h5>
-                <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="modal"
-                ></button>
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" :id="'modalNumber' + index + 'Label'">
+                    post {{ index }}
+                  </h5>
+                  <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                  ></button>
                 </div>
                 <div class="modal-body">
-                  <div>
-                  
-
-    <form>
-      <label for="target-email-id">Target Email ID</label>
-      <input
-        name="target-email-id"
-        :placeholder="user.email"
-        type="email"
-        v-model="emailId"
-        v-on:keyup="updateOutputUrl"
-      />
-      <label for="subject">Subject</label>
-      <input
-        name="subject"
-        placeholder="Subject"
-        type="text"
-        v-model="email.subject"
-        v-on:keyup="updateOutputUrl"
-      />
-      <label for="cc">CC</label>
-      <input
-        name="cc"
-        placeholder="CC"
-        type="text"
-        v-model="email.cc"
-        v-on:keyup="updateOutputUrl"
-      />
-      <label for="bcc">BCC</label>
-      <input
-        name="bcc"
-        placeholder="BCC"
-        type="text"
-        v-model="email.bcc"
-        v-on:keyup="updateOutputUrl"
-      />
-      <label for="body">Body</label>
-      <textarea
-        name="body"
-        placeholder="Body"
-        type="text"
-        v-model="email.body"
-        v-on:keyup="updateOutputUrl"
-      ></textarea>
-      <input type="hidden" id="final-link-to-copy" :value="outputUrl" />
-    </form>
-                    <input
-                    type="text"
-                    v-model="message"
-                    :placeholder="user.email"
-                    />
+                  <div
+                    class="d-flex"
+                    v-for="post in userPost"
+                    :key="post"
+                    :id="'user' + user.id"
+                  >
+                    <p>bo</p>
                   </div>
                   <div class="modal-footer">
                     <button
-                    type="button"
-                    class="btn btn-secondary"
-                    data-bs-dismiss="modal"
+                      type="button"
+                      class="btn btn-secondary"
+                      data-bs-dismiss="modal"
                     >
-                    Close
-                  </button>
-                  <button
-                  type="button"
-                  class="btn btn-primary"
-                      @click="contactMe(user.email)"
-                    >
-                     send email
+                      Close
                     </button>
                   </div>
                 </div>
@@ -143,7 +102,7 @@ img {
   width: 46vw;
   bottom: 0;
   margin: var(--ss);
-overflow: scroll;
+  overflow: scroll;
 }
 #emoters {
   overflow-x: scroll !important;
@@ -151,43 +110,58 @@ overflow: scroll;
 #emoters ::-webkit-scrollbar {
   display: block;
 }
-.contactCard{
+.contactCard {
   margin: var(--ss);
   padding: var(--ss);
   border: 2px var(--sixty) solid;
 }
-
+.modal-body {
+  display: flex;
+  height: 72vh;
+  overflow: scroll;
+}
 </style>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 
-import { thisTypeAnnotation } from '@babel/types';
-
 export default {
   mounted() {
     this.sendUserData();
-    this.updateOutputUrl();
   },
   computed: {
     Posts() {
       return this.$store.state.posts;
     },
+
     users() {
-      let allUsers=this.$store.state.users
-      let i
-      let users=[]
-      for(i=0;i<allUsers.length;i++){
-        if(allUsers[i].id!=localStorage.user_id){
-users.push(allUsers[i])
+      let allUsers = this.$store.state.users;
+      let i;
+      let users = [];
+      for (i = 0; i < allUsers.length; i++) {
+        if (allUsers[i].id != localStorage.user_id) {
+          users.push(allUsers[i]);
         }
       }
       return users;
+    },
+    userPost() {
+      let allUsers = this.$store.state.users;
+      let i;
+      let users = [];
+      let user_id = localStorage.toggledUser;
+      for (i = 0; i < allUsers.length; i++) {
+        if (allUsers[i].id != localStorage.user_id) {
+          users.push(allUsers[i]);
+        }
+        console.log(user_id);
+      }
 
+      return users;
     },
   },
-  data(){
+  data() {
     return {
       outputUrl: "Type something",
       emailId: "you@example.com",
@@ -195,8 +169,8 @@ users.push(allUsers[i])
         subject: "Hey there",
         cc: "somebody@example.com",
         bcc: "somebody.else@example.com",
-        body: "Hey there, how have you been?"
-      }
+        body: "Hey there, how have you been?",
+      },
     };
   },
   methods: {
@@ -228,35 +202,9 @@ users.push(allUsers[i])
       //     this.Posts;
       // }, 1000);
     },
-    contactMe(dataLoad){
-      console.log(dataLoad)
-      console.trace()
-    },
-    updateOutputUrl() {
-      this.outputUrl = "mailto:" + this.emailId;
-      const emailKeys = Object.keys(this.email);
-      const remaining = emailKeys.filter(
-        (key) => this.email[key].trim().length > 0
-      );
-      if (remaining.length > 0) {
-        this.outputUrl += "?";
-      }
-      this.outputUrl += remaining
-        .map((key) => `${key}=${encodeURI(this.email[key].trim())}`)
-        .join("&");
-    },
-    copyToClipboard() {
-      const finalLinkToCopy = document.querySelector("#final-link-to-copy");
-      finalLinkToCopy.setAttribute("type", "text");
-      finalLinkToCopy.select();
-      try {
-        let res = document.execCommand("copy");
-        alert("Copied to clipboard!");
-      } catch (e) {
-        alert("Unable to copy! Please select the text & copy it.");
-      }
-      finalLinkToCopy.setAttribute("type", "hidden");
-      window.getSelection().removeAllRanges();
+    passId(id) {
+      console.log(id);
+      localStorage.setItem("toggledUser", JSON.stringify(id));
     },
   },
 };
